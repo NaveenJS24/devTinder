8b8d6736aa9236b09c84b010d4cb5b35d7d0793c
Naveen JS <naveenshivan24@gmail.com>
2025-01-21 15:49:46 +0530

    multi

--- src/app.js
+++ src/app.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Naveen", lastName: "JS" });
-});
-app.post("/user", (req, res) => {
-  res.send("Data is posted 2 database successfully");
-});
-app.patch("/user", (req, res) => {
-  res.send("Data is patched");
-});
-app.delete("/user", (req, res) => {
-  res.send("Data is deleted from database");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    next();
+    console.log("res 1");
+    // res.send("send 1");
+  },
+
+  (req, res, next) => {
+    console.log("res 2");
+    // res.send("send 2");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("res 3");
+      // res.send("send 3");
+      next();
+    },
+    (req, res) => {
+      console.log("res 4");
+      res.send("send 4");
+    },
+  ]
+);
 
 app.listen(3000, (req, res) => {
   console.log("Server is created listening at port 3000");
